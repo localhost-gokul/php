26a1e424b42f23e256742b773b8c3b6058ba0ee4
gokul <devmsf7@gmail.com>
2024-09-20 18:10:22 +0530

    Added the html tags

--- exp.no_6/3-switch_statement/index.php
+++ exp.no_6/3-switch_statement/index.php
@@ -6,6 +6,25 @@
     <title>Simple Calculator</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <label>Enter first number:</label>
+        <input type="number" name="num1" required> 
+        <br><br>
+
+        <label>Enter Second number:</label>
+        <input type="number" name="num2" required> 
+        <br><br>
+
+        <label>Select Operation</label>
+        <select name="operation" required>
+            <option value="Add">Addition</option>
+            <option value="Subtract">Subraction</option>
+            <option value="Multiply">Multiply</option>
+            <option value="Divide">Divide</option>
+        </select>
+        <br><br>
+
+        <input type="submit" name="submit" value="submit" >
+    </form>
 </body>
 </html>
