41562d4284af8c134f65c0fab5942984da88aab3
gokul <devmsf7@gmail.com>
2024-09-20 18:41:46 +0530

    Added the html tags

---
+++ exp.no_6/4-for_loop/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication Table</title>
+</head>
+<body>
+    <h1>Generate Multiplication table</h1>
+    <form action="" method="post">
+        <label for="number">Enter your number :</label>
+        <input type="number" name="number" required> <br><br>
+
+        <input type="submit" value="submit" value="Generate Table">
+    </form>
+
+</body>
+</html>
